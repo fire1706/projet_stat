d9773780df700b596f1da16c147af0ab4d350b06
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-10-22 20:39:29 +0200

    Update Q1a.m

---
+++ resources/Q1a.m
@@ -0,0 +1,11 @@
+% script de la question 1a
+
+populas = population(readtable('data.csv'),24);
+% on sort la population 
+%on donne un intervall de donn?e
+maximum = max(populas.Charges(:,1))
+Interval = 0: 1000 : maximum
+% de la on sort un grpahique grace a la fonction histogram
+
+histogram(populas.Charges,Interval)
+
