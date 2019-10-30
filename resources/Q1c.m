62b823ecead3adff3c12b5a901138607f8241bd1
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-10-30 17:02:27 +0100

    17:01 push 20% battery

--- resources/Q1c.m
+++ resources/Q1c.m
@@ -1,11 +1,16 @@
 function proportion = Q1c(x)
 %% Fonction calculant la proportion de personne ayant des frais normaux
-
-
-
-
-
-
-
+%% On d?fini d'abord l'interval
+[a,b] = normal_interval(x);
+%% On recherche maintenant la proportion
+i = 1;
+cpt = 0;
+while(i <500)
+    if(x(i,1)<b && x(i,1)>a)
+        cpt =cpt+1;
+    end
+    i = i+1;
+end
+proportion = cpt/500;
 
 end
--- resources/quartiles.m
+++ resources/quartiles.m
@@ -5,7 +5,12 @@ function [first_q, second_q, third_q] = quartiles(x)
 %   FIRST_Q : 1-by-1 double
 %   SECOND_Q : 1-by-1 double
 %   THIRD_Q : 1-by-1 double
-
+%%On va d'abord sortir la boite ? moustache
+boxplot(x)
+%% Calcule des quartiles
+second_q = median(x);
+first_q = median(x(find(x<median(x))));% pour ne prendre que ce qui est entre le minimum et la m?dianne
+third_q = median(x(find(x>median(x))));% pour ne prendre que ce qui est entre le maximum et la m?dianne
 
 
 end
