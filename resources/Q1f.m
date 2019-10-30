75f9a8de8fed3e1d8034f8efd33c35269016a0f8
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-10-30 18:17:34 +0100

    18:17 push 5% battery

---
+++ resources/Q1f.m
@@ -0,0 +1,30 @@
+%% Script pour la question 1 f)
+% r?alisation des scatterplot
+% on va d'abord cr?e les deux table homme femme
+Pop = population(readtable('data.csv'), 24);
+
+Homme = zeros(2,2);
+Femme = zeros(2,2);
+
+i = 1;
+h = 1;
+f = 1;
+while(i<500)
+    if(Pop.Sex(i) == 1)
+        Homme(h,1) = Pop.Age(i);
+        Homme(h,2) = Pop.Charges(i);
+        h = h+1;
+    elseif(Pop.Sex(i) == 0)
+        Femme(f,1) = Pop.Age(i);
+        Femme(f,2) = Pop.Charges(i);
+        f = f+1;
+    end
+    i = i+1;
+end
+
+figure;
+scatter(Homme(:,1),Homme(:,2))
+figure;
+scatter(Femme(:,1),Femme(:,2))
+
+            
--- resources/correlation.m
+++ resources/correlation.m
@@ -4,7 +4,17 @@ function corr_xy = correlation(x, y)
 %   X : N-by-1 double
 %   Y : N-by-1 double
 %   CORR_XY : 1-by-1 double
-
-
-
+mx = mean(x);
+my = mean(y);
+num = 0;
+den1 = 0;
+den2 = 0;
+i = 1;
+while(i<length(x))
+    num = num + (x(i,1)-mx)*(y(i,1)-my);
+    den1 = den1 + (x(i,1)-mx)*(x(i,1)-mx);
+    den2 = den2 +(y(i,1)-my)*(y(i,1)-my);
+    i = i+1;
+end
+corr_xy = num / sqrt(den1*den2);
 end
