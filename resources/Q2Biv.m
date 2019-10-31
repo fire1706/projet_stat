2ec5aed501f014f8942c58c31bcc296b46170c76
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-10-31 18:00:41 +0100

    Create Q2Biv.m

---
+++ resources/Q2Biv.m
@@ -0,0 +1,11 @@
+%% function qui va cr?e les 500 distance de kolmogorov de tout les ?chantillons
+function [dst] = Q2Biv(X,Y) % X est la matrice comprenant les 500 ?chatillons et Y la populationd de base
+    dst = zeros(500,1);
+    i = 1;
+    while(i<=500)
+        
+        [a,b,dst(i,1)] = kstest2(X(i,:),Y);
+        i = i + 1;
+    end
+    histogram(dst);
+end
