2527d45c64d33ff3e3efd266eef7177cb54a9aef
bisthebis <bism25@hotmail.com>
2019-11-13 13:43:07 +0100

    Histogramme

--- resources/Q1a.m
+++ resources/Q1a.m
@@ -5,8 +5,11 @@ populas = population(readtable('data.csv'),24);
 % on sort la population 
 %on donne un intervall de donn?e
 maximum = max(populas.Charges(:,1))
-Interval = 0: 1000 : maximum
+Interval = 0: 1000 : maximum;
 % de la on sort un grpahique grace a la fonction histogram
 
 histogram(populas.Charges,Interval)
+xlabel('Frais hospitaliers ($)')
+ylabel('N')
+title('Histgramme des frais hospitaliers')
 
