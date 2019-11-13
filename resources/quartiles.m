09f141d5b361ede582e23a9856e45d7d915eea93
bisthebis <bism25@hotmail.com>
2019-11-13 16:19:45 +0100

    Calcul d'aberrations

--- resources/quartiles.m
+++ resources/quartiles.m
@@ -8,9 +8,11 @@ function [first_q, second_q, third_q] = quartiles(x)
 %%On va d'abord sortir la boite ? moustache
 boxplot(x)
 %% Calcule des quartiles
-second_q = median(x);
-first_q = median(x(find(x<median(x))));% pour ne prendre que ce qui est entre le minimum et la m?dianne
-third_q = median(x(find(x>median(x))));% pour ne prendre que ce qui est entre le maximum et la m?dianne
+q2 = median(x);
+q1 = median(x(find(x<median(x))));% pour ne prendre que ce qui est entre le minimum et la m?dianne
+q3 = median(x(find(x>median(x))));% pour ne prendre que ce qui est entre le maximum et la m?dianne
 
+min_abberrations = q1 - 1.5*(q3 - q1)
+max_abberrations = q3 + 1.5*(q3 - q1)
 
 end
