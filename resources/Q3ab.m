196caff29c12db84291f819ebd37e4c3cb8c4709
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-12-05 12:33:39 +0100

    here men

--- resources/Q3ab.m
+++ resources/Q3ab.m
@@ -22,7 +22,7 @@ end
 %% partie A
 meanBiais =  mean(meanBMI - meanG);
 meanVariance = var(meanBMI,1);
-V = mean((meanBMI-meanG).^2) - (meanBiais)^2; % formule pour etre sur voir si on la garde ou si on garde l'autre
+V = mean((meanBMI-meanG).^2) - (meanBiais)^2; % formule pour etre sur d'avoir bien utiliser var();
 
 
 %% parie B
